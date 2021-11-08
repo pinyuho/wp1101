4e8a99894419dfde9e6a34acda947a465a5cebd8
何品諭 <pollyho@Pollys-MacBook-Air.local>
2021-11-08 20:09:05 +0800

    update calculator features

--- hw5/src/components/Button.js
+++ hw5/src/components/Button.js
@@ -2,31 +2,77 @@ import React, { useState } from 'react';
 
 function Button (props) {
 
-    function lastIsNumber(str) {
-        return /\d/.test(str.slice(-1));
-    }
+    const ops = ['+', '-', '*', '/', '.'];
+    const allButtons = ['+', '-', '*', '/', '.', '=', 'AC',
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
     function handleClick() {
         let updatedStr = "";
-        if (props.type === "equal") {
+        if (props.name === "=") {
             try {
-                updatedStr = eval(props.toCalStr);
-                console.log(eval(props.toCalStr));
+                let value = eval(props.toCalStr).toString();
+                if (value === 'Infinity' || value === '-Infinity' || value === 'NaN') {
+                    updatedStr = "error";
+                }
+                else {
+                    updatedStr = (Math.round(value * 100000) / 100000).toString();
+                    console.log("value");
+                    console.log(value);
+                }
             } catch (e) {
                 console.log(e);
                 updatedStr = "error";
             }
             
         }
-        else if (props.type === "operands") {
-            updatedStr = lastIsNumber(props.toCalStr) 
-                ? (props.toCalStr + props.name)
-                : props.toCalStr.replace(/.$/, props.name);
+        else if (props.name === "AC") {
+            console.log("pressed AC");
+            updatedStr = "";
+        }
+        else if (props.name === "DEL") {
+            if (props.toCalStr !== ""){
+                updatedStr = props.toCalStr.substring(0, props.toCalStr.length - 1);
+            }
+        }
+        else if (props.name === "MC") {
+            console.log(props.toCalStr);
+            if (!props.toCalStr.includes(ops)){
+                props.setMC(parseInt(props.toCalStr, 10));
+                updatedStr = props.toCalStr;
+                console.log("MCCC");
+                console.log(props.mc);
+            }
+            else {
+                updatedStr = props.toCalStr + props.mc.toString();
+                console.log("MC");
+                console.log(props.mc);
+                console.log(updatedStr);
+            }
+            
+        }
+        else if (props.name === "+/-") {
+            if (props.toCalStr !== ""){
+                updatedStr = props.toCalStr * (-1);
+            }
+        }
+        else if (ops.includes(props.name) && props.toCalStr === "" ) {
+            updatedStr = "";
+        }
+        else if (ops.includes(props.name) && ops.includes(props.toCalStr.slice(-1))) {
+            updatedStr = props.toCalStr.slice(0, -1) + props.name;
+        }
+        else if (props.toCalStr.includes("error") && ops.includes(props.name)) {
+            updatedStr = "";
+        }
+        else if (props.toCalStr.includes("error")) {
+            updatedStr = props.name;
         }
         else {
             updatedStr = props.toCalStr + props.name;
+            console.log(updatedStr);
         }
-        props.setToCalStr(updatedStr);
+
+        props.setToCalStr(updatedStr.toString());
     }
 
     return ( 
--- hw5/src/containers/App.js
+++ hw5/src/containers/App.js
@@ -4,30 +4,27 @@ import Button from '../components/Button'
 
 
 const BUTTONS = {
-  'operands': ['+', '-', '*', '/'],
+  'operands': ['+', '-', '*', '/', 'AC', '+/-', 'MC', 'DEL'],
   'numbers': [7, 8, 9, 
               4, 5, 6,
               1, 2, 3,
               0],
-  'dot': '.',
-  'equal': '='
 };
-
-const BUTTON_TYPES = Object.keys(BUTTONS);
 
 const brNums = [9, 6, 3];
 
-
 function App() {
 
-  const [toCalStr, setToCalStr] = useState("0");
+  const [toCalStr, setToCalStr] = useState("");
+  const [mc, setMC] = useState(0);
 
   const operandList = BUTTONS.operands.map(operand => (
     <Button key={operand}
             name={operand} 
-            type="operands"
             toCalStr={toCalStr}
-            setToCalStr={setToCalStr}/>
+            setToCalStr={setToCalStr}
+            mc={mc}
+            setMC={setMC}/>
   ));
 
   const numberList = BUTTONS.numbers.map(number => {
@@ -35,35 +32,40 @@ function App() {
       <>
       <Button key={number}
               name={number} 
-              type="numbers"
               toCalStr={toCalStr}
-              setToCalStr={setToCalStr}/> <br/>
+              setToCalStr={setToCalStr}
+              mc={mc}
+              setMC={setMC}/> <br/>
       </>
     : <Button key={number}
               name={number} 
-              type="numbers"
               toCalStr={toCalStr}
-              setToCalStr={setToCalStr}/>
+              setToCalStr={setToCalStr}
+              mc={mc}
+              setMC={setMC}/>
     }
   );
 
 
   return (
     <div className="App">
-        <h1 className="App-display"> {toCalStr} </h1>
+        <h1 className="App-display"> {toCalStr || "0"} </h1>
         <span className="App-controls">
             {operandList} <br/>
+            
             {numberList}
             <Button key="."
                     name="."
-                    type="dot"
-                    toCalStr="{toCalStr}"
-                    setToCalStr={setToCalStr}/>
+                    toCalStr={toCalStr}
+                    setToCalStr={setToCalStr}
+                    mc={mc}
+                    setMC={setMC}/>
             <Button key="="
                     name="="
-                    type="equal" 
                     toCalStr={toCalStr}
-                    setToCalStr={setToCalStr}/>
+                    setToCalStr={setToCalStr}
+                    mc={mc}
+                    setMC={setMC}/>
         </span>
     </div>
 
